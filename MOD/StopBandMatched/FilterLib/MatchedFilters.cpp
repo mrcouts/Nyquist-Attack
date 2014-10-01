93ebd10ad82f734e6a72f0002369d6b4bd34b931
André <mrcouts@gmail.com>
2014-09-30 21:54:41 -0300

    Inicio da implementação do filtro stop band utilizando matching z transform

---
+++ MOD/StopBandMatched/FilterLib/MatchedFilters.cpp
@@ -0,0 +1,77 @@
+#include <cmath>
+#include "ModFilters.h"
+
+
+
+void SBF1(double *u, double *y, int N, double f_before, double f_now, double BW_before, double BW_now, double T, double *U_1, double *U_2, double *Y_1, double *Y_2)
+{
+	double K;
+	double A;
+	double B;
+	double C;
+	double f;
+	double BW;
+	double wc;
+	double dw;
+	double wcT;
+	double dwT;
+	
+	double y_1 = Y_1[0];
+	double u_1 = U_1[0];
+	double y_2 = Y_2[0];
+	double u_2 = U_2[0];
+	
+	/*
+	              z² - 2cos(Twc)z + 1
+	 G(z) = K ------------------------
+	                z² + Az + B 
+	*/         
+	
+	f = f_before;
+	BW = BW_before;
+	wc = 2*M_PI*f;
+	dw = 2*M_PI*BW;
+	wcT = wc*T;
+	dwT = dw*T;
+	A = exp(0.5*(-dwT + sqrt(dwT*dwT - 4*wcT*wcT))) - exp(-0.5*(dwT + sqrt(dwT*dwT - 4*wcT*wcT)));
+	B = exp(-dwT);
+	cos_wcT = cos(wcT);
+	K = (1+a+b)/(2*(1-cos_wcT));
+	
+	y[0] = -a*y_1 - b*y_2 + K*(u[0] -2*cos_wcT*u_1 + u_2);
+	
+	f = f_before + ((f_now - f_before)/(N-1))*(1);
+	BW = BW_before + ((BW_now - BW_before)/(N-1))*(1);
+	wc = 2*M_PI*f;
+	dw = 2*M_PI*BW;
+	wcT = wc*T;
+	dwT = dw*T;
+	A = exp(0.5*(-dwT + sqrt(dwT*dwT - 4*wcT*wcT))) - exp(-0.5*(dwT + sqrt(dwT*dwT - 4*wcT*wcT)));
+	B = exp(-dwT);
+	cos_wcT = cos(wcT);
+	K = (1+a+b)/(2*(1-cos_wcT));
+	
+	y[1] = -a*y[0] - b*y_1 + K*(u[1] -2*cos_wcT*u[0] + u_1);
+		
+	for (int i=2; i<=N-1; i++)
+	{
+		f = f_before + ((f_now - f_before)/(N-1))*(i);
+		BW = BW_before + ((BW_now - BW_before)/(N-1))*(i);
+		wc = 2*M_PI*f;
+		dw = 2*M_PI*BW;
+		wcT = wc*T;
+		dwT = dw*T;
+		A = exp(0.5*(-dwT + sqrt(dwT*dwT - 4*wcT*wcT))) - exp(-0.5*(dwT + sqrt(dwT*dwT - 4*wcT*wcT)));
+		B = exp(-dwT);
+		cos_wcT = cos(wcT);
+		K = (1+a+b)/(2*(1-cos_wcT));
+
+		y[i] = -a*y[i-1] - b*y[i-2] + K*(u[i] -2*cos_wcT*u[i-1] + u[i-2]);
+	}
+	
+	U_1[0] = u[N-1];
+	Y_1[0] = y[N-1];
+	U_2[0] = u[N-2];
+	Y_2[0] = y[N-2];
+	
+}
---
+++ MOD/StopBandMatched/FilterLib/MatchedFilters.h
@@ -0,0 +1,3 @@
+#include <cmath>
+
+void SBF1(double *u, double *y, int N, double f_before, double f_now, double BW_before, double BW_now, double T, double *U_1, double *U_2, double *Y_1, double *Y_2);
