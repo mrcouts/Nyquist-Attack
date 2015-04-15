0d0cfadba897e7e813a2a4e495d6215812b0730a
André <mrcouts@gmail.com>
2015-04-15 02:09:50 -0300

    Compreensão de lista no código \o/

--- Robotics/Serial.py
+++ Robotics/Serial.py
@@ -1,5 +1,6 @@
 from sympy import *
 import math
+from operator import concat
 init_printing(use_unicode=True)
 
 t = symbols('t')
@@ -32,7 +33,7 @@ def H(axis, T, dx, dy, dz):
     elif axis == '0':
         return Hz(0,dx,dy,dz)
     else:
-        print("Tah bem loko?")
+        raise ValueError("Soh pode ser string x, y, z ou 0!")
         
 def H2(axis_rot, axis_trans,T, l):
     if axis_trans == 'x':
@@ -42,7 +43,7 @@ def H2(axis_rot, axis_trans,T, l):
     elif axis_trans == 'z' :
         return H(axis_rot,T,0,0,l)
     else:
-        print("Tah bem loko?")
+        raise ValueError("Soh pode ser string x, y, z!")
         
 def MassCenter(axis, l):
     if axis == 'x':
@@ -79,71 +80,50 @@ class Serial(object):
         self.code = code
 
         #Verificando se o mecanismo tem juntas prismaticas
-        self.pflag = False
-        for i in range(self.dof):
-            self.pflag |= (str(code[i,0]) == '0')
+        self.pflag = any(str(code[i,0]) == "0" for i in range(self.dof))
 
-        if ID == '':
-            Id = ''
-        else:
-            Id = '_' + str(ID)
+        Id = '' if ID == '' else '_' + str(ID)
         
         #Coordenadas Generalizadas
-        self.qh_ = zeros(self.dof,1)
-        for i in range(self.dof):
-            if str(code[i,0]) != '0' :
-                self.qh_[i] = Function('theta_' + str(i+1) + Id )(t)
-            else:
-                self.qh_[i] = Function('d_' + str(i+1) + Id )(t)
+        self.qh_ = Matrix([Function('theta_'+str(i+1)+Id)(t) if str(code[i,0]) != '0' else 
+        	               Function('d_'    +str(i+1)+Id)(t) for i in range(self.dof) ])
                 
-        self.qo_ = zeros(3*self.dof,1)
-        for i in range(self.dof):
-            self.qo_[3*i+0] = Function('x_' + str(i+1) + Id )(t)
-            self.qo_[3*i+1] = Function('y_' + str(i+1) + Id )(t)
-            self.qo_[3*i+2] = Function('z_' + str(i+1) + Id )(t)
+        self.qo_ = Matrix(reduce(concat, [ [
+        	Function('x_' + str(i+1) + Id )(t), 
+        	Function('y_' + str(i+1) + Id )(t), 
+        	Function('z_' + str(i+1) + Id )(t)] for i in range(self.dof) ]) )
             
         self.q_ = Matrix([self.qh_,self.qo_])
                 
         #VelocIDades Generalizadas
         self.ph_ = self.qh_.diff(t)
         
-        pw_ = zeros(3*self.dof,1)
-        for i in range(self.dof):
-            pw_[3*i+0] = Function('wx_' + str(i+1) + Id )(t)
-            pw_[3*i+1] = Function('wy_' + str(i+1) + Id )(t)
-            pw_[3*i+2] = Function('wz_' + str(i+1) + Id )(t)
+        pw_ = Matrix(reduce(concat, [ [
+        	Function('wx_' + str(i+1) + Id )(t), 
+        	Function('wy_' + str(i+1) + Id )(t), 
+        	Function('wz_' + str(i+1) + Id )(t)] for i in range(self.dof) ]) )
             
-        pv_ = zeros(3*self.dof,1)
-        for i in range(self.dof):
-            pv_[3*i+0] = Function('vx_' + str(i+1) + Id )(t)
-            pv_[3*i+1] = Function('vy_' + str(i+1) + Id )(t)
-            pv_[3*i+2] = Function('vz_' + str(i+1) + Id )(t)
+        pv_ = Matrix(reduce(concat, [ [
+        	Function('vx_' + str(i+1) + Id )(t), 
+        	Function('vy_' + str(i+1) + Id )(t), 
+        	Function('vz_' + str(i+1) + Id )(t)] for i in range(self.dof) ]) )
             
         po_ = Matrix([pw_,pv_])
         p_ = Matrix([self.ph_, po_])
         
         #Matrizes de transformacao homogenea relativas  
-        vH_ = []
-        vH_.append(H2(str(code[0,0]),str(code[0,1]),self.qh_[0],0))
-        for i in range(1,self.dof):
-            if str(code[i-1,0]) != '0' :
-                vH_.append(H2(str(code[i,0]),str(code[i,1]),self.qh_[i],symbols('l_'+str(i) + Id)))
-            else:
-                vH_.append(H2(str(code[i,0]),str(code[i,1]),self.qh_[i],self.qh_[i-1]))
+        vH_ = [H2(str(code[0,0]),str(code[0,1]),self.qh_[0],0)] + [
+               H2(str(code[i,0]),str(code[i,1]),self.qh_[i],symbols('l_'+str(i) + Id)) if str(code[i-1,0]) != '0' else 
+               H2(str(code[i,0]),str(code[i,1]),self.qh_[i],self.qh_[i-1]) for i in range(1,self.dof)]
                 
         #Centros de massa nos S.C. das barras:
-        vgb_ = []
-        for i in range(self.dof):
-            if str(code[i,0]) != '0':
-                vgb_.append(MassCenter(str(code[i,1]),symbols('lg_'+str(i+1) + Id)))
-            else:
-                vgb_.append(MassCenter(str(code[i,1]),self.qh_[i] - symbols('l_'+str(i+1) + Id) +  symbols('lg_'+str(i+1) + Id)))
+        vgb_ = [MassCenter(str(code[i,1]),symbols('lg_'+str(i+1) + Id)) if str(code[i,0]) != '0' else 
+                MassCenter(str(code[i,1]),self.qh_[i] - symbols('l_'+str(i+1) + Id) +  symbols('lg_'+str(i+1) + Id)) 
+                for i in range(self.dof)]
 
         #Posicao do efetuador no S.C. da ultima barra:
-        if str(code[self.dof-1,0]) != '0' :
-            self.xb_ = MassCenter(str(code[self.dof-1,1]),symbols('l_'+str(self.dof) + Id))
-        else:
-            self.xb_ = MassCenter(str(code[i,1]),self.qh_[i])
+        self.xb_ = (MassCenter(str(code[self.dof-1,1]),symbols('l_' + str(self.dof) + Id)) if str(code[self.dof-1,0]) != '0' else 
+        	        MassCenter(str(code[i,1]),self.qh_[i]))
             
         #Matrizes de transformacao homogenea absolutas
         vI_ = []
@@ -155,27 +135,19 @@ class Serial(object):
         self.R_ = h2rot(vI_[self.dof-1])
             
         #Centros de massa no S.C. N:
-        vgn_ = []
-        for i in range(self.dof):
-            vgn_.append(h2vec(simplify(vI_[i]*vec2h(vgb_[i]))))
+        vgn_ = [h2vec(simplify(vI_[i]*vec2h(vgb_[i]))) for i in range(self.dof)]
 
         #Posicao do efetuador no S.C. N:
         self.xn_ = h2vec(simplify(vI_[self.dof-1]*vec2h(self.xb_)))
             
         #Velocidades dos centros de massa:
-        self.vv_ = []
-        for i in range(self.dof):
-            self.vv_.append(simplify(vgn_[i].diff(t)))
+        self.vv_ = [simplify(vgn_[i].diff(t)) for i in range(self.dof)]
             
         #Matriz S:
-        vS_ = []
-        for i in range(self.dof):
-            vS_.append(simplify(h2rot(vI_[i]).T * h2rot(vI_[i]).diff(t)))
+        vS_ = [simplify(h2rot(vI_[i]).T * h2rot(vI_[i]).diff(t)) for i in range(self.dof)]
             
         #Velocidades angulares das barras:
-        self.vw_ = []
-        for i in range(self.dof):
-            self.vw_.append(Matrix([vS_[i][2,1],vS_[i][0,2],vS_[i][1,0]]))
+        self.vw_ = [Matrix([vS_[i][2,1],vS_[i][0,2],vS_[i][1,0]]) for i in range(self.dof)]
             
         #Vetor po_ em funcao de ph_:
         w_ = self.vw_[0]
@@ -189,16 +161,11 @@ class Serial(object):
         _Po_ = Matrix([w_,v_])
 
         #Indices dos elementos de _Po_ nao nulos      
-        nonzerolist = []
-        for i in range(6*self.dof):
-            if _Po_[i] != 0:
-                nonzerolist.append(i)
+        nonzerolist = [i for i in range(6*self.dof) if _Po_[i] != 0] 
         
         #Indices dos elementos de _p_ nao nulos   
-        nonzerolist2 = range(self.dof)
-        for i in nonzerolist:
-            nonzerolist2.append(i + self.dof)
-                
+        nonzerolist2 = range(self.dof) + [i + self.dof for i in nonzerolist]
+
         _po_ = _Po_[nonzerolist, :]
         self.po_ = po_[nonzerolist, :]
         self.p_ = Matrix([self.ph_,self.po_])
@@ -213,21 +180,18 @@ class Serial(object):
         self.b_ = simplify(-self.A_.diff(t)*self.p_)
         
         #energia de aceleracoes:
-        s = 0
-        for i in range(self.dof):
-            s += ( 
-            	symbols('m_' + str(i+1) + Id)*( pv_[3*i].diff(t)**2 + pv_[3*i+1].diff(t)**2 + pv_[3*i+2].diff(t)**2 ) + 
-            	symbols('Jx'+str(i+1) + Id)*pw_[3*i+0].diff(t)**2 + 
-            	symbols('Jy'+str(i+1) + Id)*pw_[3*i+1].diff(t)**2 + 
-            	symbols('Jz'+str(i+1) + Id)*pw_[3*i+2].diff(t)**2 + 
-            	2*pw_[3*i+0].diff(t)*(symbols('Jz'+str(i+1) + Id)-symbols('Jy'+str(i+1) + Id))*pw_[3*i+2]*pw_[3*i+1] + 
-            	2*pw_[3*i+1].diff(t)*(symbols('Jx'+str(i+1) + Id)-symbols('Jz'+str(i+1) + Id))*pw_[3*i+0]*pw_[3*i+2] + 
-            	2*pw_[3*i+2].diff(t)*(symbols('Jy'+str(i+1) + Id)-symbols('Jx'+str(i+1) + Id))*pw_[3*i+1]*pw_[3*i+0] )/2
+        s = sum([
+           	(symbols('m_' + str(i+1) + Id)*( pv_[3*i].diff(t)**2 + pv_[3*i+1].diff(t)**2 + pv_[3*i+2].diff(t)**2 ) + 
+           	 symbols('Jx'+str(i+1) + Id)*pw_[3*i+0].diff(t)**2 + 
+           	 symbols('Jy'+str(i+1) + Id)*pw_[3*i+1].diff(t)**2 + 
+           	 symbols('Jz'+str(i+1) + Id)*pw_[3*i+2].diff(t)**2 + 
+           	 2*pw_[3*i+0].diff(t)*(symbols('Jz'+str(i+1) + Id)-symbols('Jy'+str(i+1) + Id))*pw_[3*i+2]*pw_[3*i+1] + 
+           	 2*pw_[3*i+1].diff(t)*(symbols('Jx'+str(i+1) + Id)-symbols('Jz'+str(i+1) + Id))*pw_[3*i+0]*pw_[3*i+2] + 
+           	 2*pw_[3*i+2].diff(t)*(symbols('Jy'+str(i+1) + Id)-symbols('Jx'+str(i+1) + Id))*pw_[3*i+1]*pw_[3*i+0] )/2 
+           	 for i in range(self.dof) ])
             
         #energia potencial
-        ep = 0
-        for i in range(self.dof):
-            ep += symbols('m_' + str(i+1) + Id)*symbols('g')*self.qo_[3*i+2]
+        ep = sum([symbols('m_' + str(i+1) + Id)*symbols('g')*self.qo_[3*i+2] for i in range(self.dof)])
     
         #Matrizes da dinamica
         self.M_ = (Matrix([s]).jacobian(self.p_.diff(t))).jacobian(self.p_.diff(t))
@@ -242,9 +206,7 @@ class Serial(object):
         
         #Balanceamento estatico
         if self.pflag == False:
-            lgx = []
-            for i in range(self.dof):
-                lgx.append(symbols('lg_'+str(i+1) + Id))
+            lgx = [symbols('lg_'+str(i+1) + Id) for i in range(self.dof)]
             self.StaticBal = solve(self.gh_, lgx)
             
             self.gh_sb_ = simplify(self.gh_.subs(self.StaticBal))
@@ -302,7 +264,6 @@ class Serial(object):
         print " "
         print "gh_sb_ = "
         pprint(self.gh_sb_)
-       
-
+    
 RR = Serial("RR", '', Matrix([['x','x'],['y','y']]).T)
 RR.description()
