b7d22067cfbae07058ae470090c333637f6dfb9c
André <mrcouts@gmail.com>
2015-04-16 18:10:57 -0300

    Agora em arquivos separados. Balanceamento de 3 mecanismos diferentes

---
+++ Robotics/RxRxRz.py
@@ -0,0 +1,38 @@
+from Serial import *
+
+RR = Serial("RR", '0', Matrix([['x','x','z'],['y','y','y']]).T)
+mot = Motor("mot1",'1')
+
+ph_ = RR.ph_
+po_ = mot.p_
+p_ = Matrix([ph_,po_])
+M_ = diag(RR.Mh_sb_, mot.M_)
+v_ = Matrix([RR.vh_sb_, mot.v_])
+g_ = Matrix([RR.gh_sb_, mot.g_])
+phi_ = po_ - Matrix([symbols('beta')*ph_[1], RR.vw_[0][0]+mot.ph_[0],RR.vw_[0][1],RR.vw_[0][2],RR.vv_[0][0],RR.vv_[0][1],RR.vv_[0][2]]).subs(RR.StaticBal)
+Ah_ = phi_.jacobian(ph_)
+Ao_ = phi_.jacobian(po_)
+C_ = Matrix([eye(3),simplify(-Ao_**-1 * Ah_)])
+Mh_ = simplify( (C_.T * M_ * C_).subs(symbols('Jy'+str(2+1) + '_' + str(0)), symbols('Jx'+str(2+1) + '_' + str(0))) )
+vh_ = simplify(  (C_.T * ( M_ * C_.diff(t)*ph_ + v_ ) ).subs(symbols('Jy'+str(2+1) + '_' + str(0)), symbols('Jx'+str(2+1) + '_' + str(0))) )
+gh_ = simplify(C_.T *g_)
+Sol = solve( Matrix([Mh_[1,0]]) , [symbols('beta')] )
+Mh_db_ = simplify(Mh_.subs(Sol))
+vh_db_ = simplify(vh_.subs(Sol))
+gh_db_ = simplify(gh_.subs(Sol))
+
+pprint(p_)
+pprint(M_)
+pprint(v_)
+pprint(g_)
+pprint(phi_)
+pprint(Ah_)
+pprint(Ao_)
+pprint(C_)
+pprint(Mh_)
+pprint(vh_)
+pprint(gh_)
+pprint(Sol)
+pprint(Mh_db_)
+pprint(vh_db_)
+pprint(gh_db_)
