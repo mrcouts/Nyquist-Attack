5c82dc2b5d3c8f91eabddc5e39fd602ece257856
André <mrcouts@gmail.com>
2015-04-28 17:26:13 -0300

    O grosso da parte 3 terminada \o/

--- Robotics/ArtigoBalance/adaptive.sh
+++ Robotics/ArtigoBalance/adaptive.sh
@@ -1,10 +1,10 @@
 # adapta.sh
-latex --src -interaction=nonstopmode [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms
-bibtex [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms
-makeindex [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms.nlo -s nomencl.ist -o [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms.nls
-latex --src -interaction=nonstopmode [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms
-# latex --src -interaction=nonstopmode [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms
-# pdflatex -synctex=1 -interaction=nonstopmode [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms.tex
-pdflatex -synctex=1 [ORSINO,\ COUTINHO,\ HESS-COELHO]\ Dynamic\ modelling\ and\ control\ of\ balanced\ parallel\ mechanisms.tex
+latex --src -interaction=nonstopmode Artigo
+bibtex Artigo
+makeindex Artigo.nlo -s nomencl.ist -o Artigo.nls
+latex --src -interaction=nonstopmode Artigo
+# latex --src -interaction=nonstopmode Artigo
+# pdflatex -synctex=1 -interaction=nonstopmode Artigo.tex
+pdflatex -synctex=1 Artigo.tex
 # rm -f *.aux *.log 
 rm -f *.blg *.dvi *.ps *.lot *.lof *.idx *.nlo *.nls *.ilg *.out *.toc *.bbl
