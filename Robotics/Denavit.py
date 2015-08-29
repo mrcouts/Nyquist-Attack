d204f7cfe43b8922c472503ed245c00cd0b25a72
André <mrcouts@gmail.com>
2015-08-29 01:46:24 -0300

    Algumas melhoras no codigo

--- Robotics/Denavit.py
+++ Robotics/Denavit.py
@@ -44,10 +44,15 @@ H_d2 = lambda lista:H_d(lista[0],lista[1],lista[2],lista[3])
 H_d3 = lambda mat:[H_d2(mat[i,:]) for i in range(mat.rows)]
     
 
-a = Matrix([0,0,0,0,0,0])
-alpha = Matrix([pi/2,-pi/2,pi/2,-pi/2,pi/2,0])
-d = Matrix([0,symbols('l_1')+symbols('l_2'),0,symbols('l_3')+symbols('l_4'),0,symbols('l_5')+symbols('l_6')])
-theta = Matrix([symbols('theta_1')+pi/2,symbols('theta_2'),symbols('theta_3'),symbols('theta_4'),symbols('theta_5'),symbols('theta_6')])
+#a = Matrix([0,0,0,0,0,0])
+#alpha = Matrix([pi/2,-pi/2,pi/2,-pi/2,pi/2,0])
+#d = Matrix([0,symbols('l_1')+symbols('l_2'),0,symbols('l_3')+symbols('l_4'),0,symbols('l_5')+symbols('l_6')])
+#theta = Matrix([symbols('theta_1')+pi/2,symbols('theta_2'),symbols('theta_3'),symbols('theta_4'),symbols('theta_5'),symbols('theta_6')])
+
+a = Matrix([0,0,0,0])
+alpha = Matrix([pi/2,-pi/2,pi/2,0])
+d = Matrix([0,symbols('l_1')+symbols('l_2'),0,symbols('l_3')+symbols('l_4')])
+theta = Matrix([symbols('theta_1')+pi/2,symbols('theta_2'),symbols('theta_3'),symbols('theta_4')])
 
 Denavit_matrix = Matrix([a.T,alpha.T,d.T,theta.T]).T
 
@@ -66,6 +71,6 @@ O_ = [Matrix([0,0,0])] + [vI_[i][0:3,3] for i in range(Denavit_matrix.rows)]
 dO_ = [simplify(O_[Denavit_matrix.rows] - O_[i]) for i in range(Denavit_matrix.rows)]
 
 Jv_ = simplify(Matrix( [z_[i].cross(dO_[i]).T for i in range(Denavit_matrix.rows) ]).T )
-Jw_ = simplify( vI_[5][0:3,0:3].T*Matrix( [z_[i] for i in range(Denavit_matrix.rows) ]).T )
+Jw_ = simplify( vI_[Denavit_matrix.rows-1][0:3,0:3].T*Matrix( [z_[i].T for i in range(Denavit_matrix.rows) ]).T )
 
 J_ = Matrix([Jv_,Jw_])
