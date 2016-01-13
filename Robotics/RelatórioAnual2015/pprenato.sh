365aa7f1c234ec2e27bfb59fc6efbe7765d5a55e
André <mrcouts@gmail.com>
2016-01-13 02:02:09 -0200

    Versão final com lista de símbolos \o/

--- Robotics/RelatórioAnual2015/pprenato.sh
+++ Robotics/RelatórioAnual2015/pprenato.sh
@@ -1,9 +1,16 @@
 #relrenato
 latex --src -interaction=nonstopmode relrenato 
-bibtex relrenato
+#bibtex relrenato
+makeindex relrenato
+#makeindex relrenato.glo -s relrenato.ist -o relrenato.gls 
+makeindex relrenato.cno -s relrenato.ist -o relrenato.cns 
+makeindex relrenato.syo -s relrenato.ist -o relrenato.sys
+
 latex --src -interaction=nonstopmode relrenato
-latex --src -interaction=nonstopmode relrenato 
+#latex --src -interaction=nonstopmode relrenato 
 pdflatex -synctex=1 relrenato.tex
+#xelatex -synctex=1 -interaction=nonstopmode relrenato
+#xelatex -synctex=1 relrenato
 rm -f *.aux *.log *.blg *.dvi *.ps *.toc *.lot *.lof *.idx *.nlo *.nls *.ilg *.out *.fls
 #
 #relatorio_renato_maia_matarazzo_orsino
